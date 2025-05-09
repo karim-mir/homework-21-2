4ac27f645efdd7448aba765d0fc57cf43e8a3749
NazKar@Nazir <karimov.nazir00@yandex.ru>
2025-05-09 09:55:20 +0500

    Add gitignore and bootstrap.min.css Разделил проект на соответствующие вкладки: Главная, Каталог, категории, Контакты. Добавил боковую панель также в отдельный файл Реализовал статичные страницы для всех вкладок, также добавил файл со стилями. Начало работы с ДЗ 22 Добавление шаблонов страницы "Главная" и "Контакты", настройка маршрутизации приложений "Catalog" Настройка шаблонов, контроллеров и маршрутизация для контроллеров Добавил файл README.md Выполнил ДЗ 23 Исправил недочеты ДЗ 23 Выполнил ДЗ 24 Дополнил страницу Контакты и добавил ограничение на 100 символов в описании товара. Доработал скрипт для добавления тестовых продуктов. Выполнил ДЗ 25, добавил приложение Блогов. Добавил счетчик просмотров статей, фильтрацию опубликованных статей и перенаправление после редактирования статей. Перевел контроллер контактов на CBV. Добавил кнопку перехода на функционал блогов на главную страницу. Выполнил ДЗ 26.1

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,4 +1,58 @@
 from django import forms
+from django.forms import ModelForm, BooleanField
+from catalog.models import Product
+
+FORBIDDEN_WORDS = [
+    "казино",
+    "криптовалюта",
+    "крипта",
+    "биржа",
+    "дешево",
+    "бесплатно",
+    "обман",
+    "полиция",
+    "радар"
+]
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs["class"] = "form-check-input"
+            else:
+                field.widget.attrs["class"] = "form-control"
+
+
+class ProductForm(StyleFormMixin, ModelForm):
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+    def clean(self):
+        cleaned_data = super().clean()
+        name = cleaned_data.get("name")
+        description = cleaned_data.get("description")
+        if name:
+            self.validate_forbidden_words(name)
+        if description:
+            self.validate_forbidden_words(description)
+        return cleaned_data
+
+    def validate_forbidden_words(self, value):
+        for word in FORBIDDEN_WORDS:
+            if word.lower() in value.lower():
+                raise forms.ValidationError(f"Слово '{word}' запрещено использовать.")
+
+    def clean_price(self):
+        price = self.cleaned_data.get("price")
+
+        if price is not None and price < 0:
+            raise forms.ValidationError("Цена не может быть отрицательной.")
+        elif price is not None and price == 0:
+            raise forms.ValidationError("Цена не может быть равна нулю.")
+
+        return price
 
 
 class ContactForm(forms.Form):
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from catalog.models import Product
+from catalog.forms import ProductForm
 from django.shortcuts import render, get_object_or_404
 
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
@@ -19,18 +20,22 @@ class ProductDetailView(DetailView):
 class ProductCreateView(CreateView):
     model = Product
     template_name = "catalog/product_form.html"
-    fields = ("name", "description", "photo", "category", "price")
+    form_class = ProductForm
     success_url = reverse_lazy("catalog:catalog_list")
 
 
 class ProductUpdateView(UpdateView):
     model = Product
     template_name = "catalog/product_form.html"
-    fields = ("name", "description", "photo", "category", "price")
+    form_class = ProductForm
     success_url = reverse_lazy("catalog:catalog_list")
 
     def get_success_url(self):
         return reverse("catalog:catalog_detail", args=[self.kwargs.get("pk")])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
 
 
 class ProductDeleteView(DeleteView):
@@ -41,3 +46,4 @@ class ProductDeleteView(DeleteView):
 
 class ContactView(TemplateView):
     template_name = "catalog/contact.html"
+    success_url = reverse_lazy("catalog:contact_success.html")
